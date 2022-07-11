7081bad2913fc3ce1e21e236eab965c847458918
Esteban <esteban1.redon2@gmail.com>
2022-07-11 13:26:20 -0300

    Se terminó la sección del portfolio

--- views/index.php
+++ views/index.php
@@ -1,6 +1,6 @@
 <div class="about-container container-sm section">
   <div class="about">
-    <h2 class="tx-start title">About title...</h2>
+    <h2 class="tx-start">About title...</h2>
     <div class="about-text-container">
       <div class="text-column">
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Earum aperiam distinctio sapiente quam laudantium id dolore, inventore repellendus velit, tempore labore tempora ipsa nostrum qui praesentium iusto vitae hic unde.</p>
@@ -16,7 +16,7 @@
   <div class="img-container"></div>
   <div class="info-container">
     <div class="info-content">
-      <h2>About subtitle...</h2>
+      <h2 class="margin-bottom-none">About subtitle...</h2>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, veniam.</p>
       <a href="#">Link</a>
     </div>
@@ -79,6 +79,60 @@
       </div>
     </div>
   </div>
+</section>
+
+<section class="portfolio-container section container-sm">
+  <div class="portfolio">
+    <h2>Portfolio</h2>
+    <div class="articles-container">
+      <article class="proyect proyect1">
+        <a href="">
+          <div class="img-container">
+            <img src="build/img/portfolio2.webp" alt="Proyect image">
+          </div>
+          <div class="proyect-text">
+            <h3>Proyect title</h3>
+            <p class="proyect-subtitle1">Proyect subtitle</p>
+          </div>
+        </a>
+      </article>
+      <article class="proyect proyect2">
+        <div class="img-container">
+          <a href="">
+            <div class="img-container">
+              <img src="build/img/portfolio1.webp" alt="Proyect image">
+            </div>
+            <div class="proyect-text">
+              <h3>Proyect title</h3>
+              <p class="proyect-subtitle2">Proyect subtitle</p>
+            </div>
+          </a>
+      </article>
+      <article class="proyect proyect3">
+        <a href="">
+          <div class="img-container">
+            <img src="build/img/portfolio1.webp" alt="Proyect image">
+          </div>
+          <div class="proyect-text">
+            <h3>Proyect title</h3>
+            <p class="proyect-subtitle3">Proyect subtitle</p>
+          </div>
+        </a>
+      </article>
+      <article class="proyect proyect4">
+        <a href="">
+          <div class="img-container">
+            <img src="build/img/portfolio1.webp" alt="Proyect image">
+          </div>
+          <div class="proyect-text">
+            <h3>Proyect title</h3>
+            <p class="proyect-subtitle4">Proyect subtitle</p>
+          </div>
+        </a>
+      </article>
+    </div>
+  </div>
+
 </section>
 
 <?php
